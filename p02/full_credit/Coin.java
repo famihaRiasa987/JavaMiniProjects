b4d2ddefd4dad0adc1fb2a401f85eadadb4950be
Famiha Riasa <fxr1764@mavs.uta.edu>
2024-09-02 23:02:31 -0500

    commit3 adding Purse class

--- p02/full_credit/Coin.java
+++ p02/full_credit/Coin.java
@@ -26,6 +26,33 @@ public class Coin{
 
     public int getYear() {
         return year;
+    }}
+class Purse{
+    public static void main(String[] args){
+        Coin[] purse = new Coin[5];
+        purse[0] = new Coin(Denomination.PENNY, 1900);
+        purse[1] = new Coin(Denomination.DIME, 1860);
+        purse[2] = new Coin(Denomination.NICKEL, 1950);
+        purse[3] = new Coin(Denomination.PENNY, 1980);
+        purse[4] = new Coin(Denomination.QUARTER, 1880);
+        double totalValue = 0.0;
+        int earliestYear = Integer.MAX_VALUE;
+        int latestYear = Integer.MIN_VALUE;
+        for(Coin coin : purse){
+            totalValue += coin.getValue();
+            int year = coin.getYear();
+            if (year < earliestYear) {
+                earliestYear = year;
+            }
+            if (year > latestYear) {
+                latestYear = year;
+            }
+        }
+
+        System.out.println("You have: $" + String.format("%.2f",totalValue)+ " coins");
+        System.out.println("Earliest year is : " + earliestYear + " to latest year is: " + latestYear);
     }
 
 }
+
+
