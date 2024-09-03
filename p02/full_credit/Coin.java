2e791300bb3b3cdb946489da0e9922f8d89c79d3
Famiha Riasa <fxr1764@mavs.uta.edu>
2024-09-02 22:48:29 -0500

    commit1

---
+++ p02/full_credit/Coin.java
@@ -0,0 +1,26 @@
+enum Denomination {
+    PENNY,
+    NICKEL,
+    DIME,
+    QUARTER
+}
+public class Coin{
+    private Denomination denomination;
+    private int year;
+    
+
+    public Coin(Denomination denomination, int year) {
+        this.denomination = denomination;  
+        this.year = year;
+    }
+
+   public double getValue() {
+    return switch (denomination) {
+        case PENNY -> 0.01;
+        case NICKEL -> 0.05;
+        case DIME -> 0.10;
+        case QUARTER -> 0.25;
+    };
+    
+}
+}
