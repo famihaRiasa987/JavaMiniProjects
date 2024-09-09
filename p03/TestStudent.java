0d8df796bcbd3f6d6e5f70a7c5ce7dc66a76d347
Famiha Riasa <fxr1764@mavs.uta.edu>
2024-09-09 06:19:49 -0500

    Finished TestStudent

--- p03/TestStudent.java
+++ p03/TestStudent.java
@@ -8,18 +8,18 @@ public class TestStudent{
     
     }
 
-    try {
+    try{
             Student studentObj1 = new Student("Famiha", 1001981764, "fxr1764@gmail.com");
             System.err.println("FAIL: No exception thrown for invalid email");
             failed = true;
         }
-        catch (IllegalArgumentException e){
+        catch(IllegalArgumentException e){
             if (!e.getMessage().equals("Non-UTA email address: fxr1764@gmail.com")) {
                 System.err.println("FAIL: Incorrect exception message. Expected: \"Non-UTA email address: fxr1764@gmail.com\" Actual: \"" + e.getMessage() + "\"");
                 failed = true;
             }
         }
-        catch (Exception e) {
+        catch(Exception e) {
             
             System.err.println("FAIL: Unexpected exception: " + e);
             failed = true;
@@ -29,7 +29,7 @@ public class TestStudent{
             System.err.println("FAIL: No exception thrown for invalid email");
             failed = true;
         } 
-        catch (IllegalArgumentException e){
+        catch(IllegalArgumentException e){
             if (!e.getMessage().equals("Non-UTA email address: famiha.gmail.com")) {
                 System.err.println("FAIL: Incorrect exception . Expected: \"Non-UTA email address: famiha.gmail.com\" Actual: \"" + e.getMessage() + "\"");
                 failed = true;
@@ -40,8 +40,17 @@ public class TestStudent{
             failed = true;
         }
 
-        if (failed)
-        {
+        Media media = new Media("Title", "URL");
+        Student studentObj3 = new Student("Famiha", 1001981764, "fxr1764@mavs.uta.edu"); 
+
+        if(!(studentObj3.requestMedia(media).equals("Playing Title (URL)"))){
+            System.err.println("FAIL: expected: Playing Title (URL) actual: " + studentObj3.requestMedia(media));
+            failed = true;
+        }
+
+
+        if(failed){
+
             System.exit(1);
         }
         else 
@@ -49,15 +58,11 @@ public class TestStudent{
             System.exit(0);
                             
         }
-    }
- 
-    
-
-
-
 
 
 
     }
-    
 
+
+    }
+    
