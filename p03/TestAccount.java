2b97fc6ed765c6081b67be4b41612550d87091a9
Famiha Riasa <fxr1764@mavs.uta.edu>
2024-09-09 03:56:48 -0500

    regression for Account class

---
+++ p03/TestAccount.java
@@ -0,0 +1,22 @@
+public class TestAccount{
+ public static void main(String[] args) {
+    boolean failed = false;
+    Account myaccount1 = new Account();
+    Account myaccount2 = new Account();
+
+
+    if(myaccount1.getAccountNumber!=1 & myaccount2.getAccountNumber!=2){
+        System.err.println("Fail: Expected Account Numbers 1 and 2")
+        System.err.println("Actual Account Numbers: "+ myaccount1.getAccountNumber()+" and"+ myaccount2.getAccountNumber());
+        failed = true;
+    }
+
+    if(failed){
+        exit(1);
+    }
+    else{
+        exit(0)
+    }
+    
+}
+}
--- p03/TestMedia.java
+++ p03/TestMedia.java
@@ -1,23 +1,23 @@
 public class TestMedia {
     public static void main(String[] args) {
-        boolean failed = false; // To track if any test fails
+        boolean failed = false; 
 
         Media media1 = new Media("Dog", "Dog.com");
         if (!media1.toString().equals("Dog (Dog.com)")) {
             System.err.println("FAIL: Expected String: Dog (Dog.com), Actual String: " + media1.toString());
-            failed = true; // Set failed to true if test fails
+            failed = true; 
         }
 
         Media media2 = new Media("Dog", "");
         if (!media2.toString().equals("Dog ()")) {
             System.err.println("FAIL: Missing URL. Expected String: Dog (), Actual String: " + media2.toString());
-            failed = true; // Set failed to true if test fails
+            failed = true; 
         }
 
         Media media3 = new Media("", "Dog.com");
         if (!media3.toString().equals(" (Dog.com)")) {
             System.err.println("FAIL: Missing title. Expected String:  (Dog.com), Actual String: " + media3.toString());
-            failed = true; // Set failed to true if test fails
+            failed = true; 
         }
 
         Media media4 = new Media("", "");
