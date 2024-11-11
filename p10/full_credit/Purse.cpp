a666e9a373869c3dd5074f8280971c0b2251f41c
Famiha Riasa <fxr1764@mavs.uta.edu>
2024-11-10 23:40:00 -0600

    push <<, >>

--- p10/full_credit/Purse.cpp
+++ p10/full_credit/Purse.cpp
@@ -6,11 +6,25 @@ Purse::Purse(int pounds, int shillings, int pence)
     rationalize();
  
 }
-    ostream& operator<<(ostream& ost , const Purse& purse){
+    ostream& operator<< (ostream& ost , const Purse& purse){
 
         ost << "£" << purse._pounds <<" "<< purse._shillings <<"s" << purse._pence <<"d";
         return ost; 
 
     }
+
+    istream& operator>> (istream& ist, Purse& purse){
+        ist >> purse._pounds >> purse._shillings >> purse._pence;
+        return ist;
+
+    }
+
+    Purse& Purse::operator++(){
+        _pence++;
+        rationalize();
+        return *this;
+    }
+
+
 
 
--- p10/full_credit/Purse.h
+++ p10/full_credit/Purse.h
@@ -6,6 +6,7 @@
 
 using std::ostream;
 using std::istream;
+
 class Purse {
     int _pounds;
     int _shillings;
@@ -17,10 +18,11 @@ public:
     Purse(int pounds = 0, int shillings = 0, int pence = 0);
 
     friend ostream& operator<< (ostream& out, const Purse& purse);
-
     friend istream& operator>> (istream& ist, Purse& purse);
+    //auto operator<=>(const Purse& purse) const = default;
 
-
+    Purse& operator++();
+    Purse operator++(int);
 
 
 };
