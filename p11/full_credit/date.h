5060a4cbefe35963ebfdf85387edf690071cdf01
Famiha Riasa <fxr1764@mavs.uta.edu>
2024-11-16 02:52:53 -0600

    finally works

--- p11/full_credit/date.h
+++ p11/full_credit/date.h
@@ -8,14 +8,11 @@ class Date {
 private:
     int _year, _month, _day;
 
-    // Helper function for comparisons
     int compare(const Date& rhs) const;
 
 public:
-    // Constructor with default values (January 1, 1970)
     Date(int year = 1970, int month = 1, int day = 1);
 
-    // Comparison operators
     bool operator==(const Date& rhs) const;
     bool operator!=(const Date& rhs) const;
     bool operator<(const Date& rhs) const;
@@ -23,11 +20,9 @@ public:
     bool operator>(const Date& rhs) const;
     bool operator>=(const Date& rhs) const;
 
-    // Friend function for outputting Date in YYYY/MM/DD format
     friend std::ostream& operator<<(std::ostream& os, const Date& date);
 
-    // Friend function for reading input in the format "YYYY M D"
     friend std::istream& operator>>(std::istream& is, Date& date);
 };
 
-#endif // DATE_H
+#endif 
